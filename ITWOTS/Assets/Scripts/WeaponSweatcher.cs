b45dc280e4e9a964a529afc13bb5b80dd49d6829
d1mKaKiselev <polizugh241@gmail.com>
2023-12-24 14:18:33 +0300

    fixed switcher weapons

--- ITWOTS/Assets/Scripts/WeaponSweatcher.cs
+++ ITWOTS/Assets/Scripts/WeaponSweatcher.cs
@@ -6,7 +6,7 @@ public class WeaponSweatcher : MonoBehaviour
 {
     public GameObject[] weapons;
     private int currentWeaponIndex = 0;
-    private KeyCode[] KeyCodes = { KeyCode.Keypad0, KeyCode.Keypad1, KeyCode.Keypad2, KeyCode.Keypad3 };
+    private KeyCode[] KeyCodes = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };
 
     // Start is called before the first frame update
     void Start()
@@ -17,25 +17,33 @@ public class WeaponSweatcher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < 4;i++)
+        for(int i = 0; i < 4; i++)
         {
             if (Input.GetKeyDown(KeyCodes[i]))
             {
                 SelectWeapon(i);
             }
         }
-    } 
+    }
+    
     void SelectWeapon(int index)
     {
         for (int i = 0; i < weapons.Length; i++)
         {
             if (i == index)
             {
-                weapons[i].SetActive(true);
+                Debug.Log($"{i} - {true}");
+                if (weapons[i] != null)
+                {
+                    weapons[i].SetActive(true);
+                }
             }
             else
             {
-                weapons[i].SetActive(false);
+                if (weapons[i] != null)
+                {
+                    weapons[i].SetActive(false);
+                }
             }
         }
         currentWeaponIndex = index;
